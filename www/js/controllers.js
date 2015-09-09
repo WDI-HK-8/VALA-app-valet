306abf482a1ba92c5676aa743b99d48631ecc249
weijefffrey <jeffreycswei@gmail.com>
2015-09-09 16:18:06 +0800

    feat(google_map): google map integration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -87,6 +87,47 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('HomeCtrl', function($scope, $http){
+.controller('HomeCtrl', function($scope, $http, nemSimpleLogger, uiGmapGoogleMapApi){
+  nemSimpleLogger.doLog = true; //default is true
+  nemSimpleLogger.currentLevel = nemSimpleLogger.LEVELS.debug
   
+  $scope.myLocation = {
+    lng : '',
+    lat: ''
+  }
+   
+  $scope.drawMap = function(position) {
+ 
+    //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
+    $scope.$apply(function() {
+      $scope.myLocation.lng = position.coords.longitude;
+      $scope.myLocation.lat = position.coords.latitude;
+ 
+      $scope.map = {
+        center: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        },
+        zoom: 14,
+        pan: 1
+      };
+ 
+      $scope.marker = {
+        id: 0,
+        coords: {
+          latitude: $scope.myLocation.lat,
+          longitude: $scope.myLocation.lng
+        }
+      }; 
+       
+      $scope.marker.options = {
+        draggable: false,
+        labelContent: "lat: " + $scope.marker.coords.latitude + '<br/> ' + 'lon: ' + $scope.marker.coords.longitude,
+        labelAnchor: "80 120",
+        labelClass: "marker-labels"
+      };  
+    });
+  }
+ 
+  navigator.geolocation.getCurrentPosition($scope.drawMap); 
 })
